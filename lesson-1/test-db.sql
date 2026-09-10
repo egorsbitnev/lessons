b18d66fa1a8bc1e56bac6f83d21e82ae66ee72d3
Егор <egorsbitnev50@gmail.com>
2026-09-10 11:11:29 +0300

    add table

---
+++ lesson-1/test-db.sql
@@ -0,0 +1,8 @@
+-- Этот код создаст в базе данных таблицу
+CREATE TABLE persons(
+    id INT,
+    name VARCHAR(255)
+);--Этот код добавит в таблицу 2 записи
+INSERT INTO persons (id,name)
+VALUES (0,'Tomas'),(1,'Martin');
+SELECT id, name FROM persons;
